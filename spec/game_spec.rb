0b56b3e43a1ed1bebe07cf379319dc1ad7906b3f
Alfonso Alamos <alfonso.informatico@gmail.com>
2020-08-27 22:39:21 -0400

    Game 1 first pitch test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require './game.rb'
 
 describe Game do
     game = Game.new
-  it "test" do
-      expect(true).to eq(true)
-  end
+    it "Game 1 first pitch" do
+        expect(game.roll(1)).to eq(1)
+    end
 end
